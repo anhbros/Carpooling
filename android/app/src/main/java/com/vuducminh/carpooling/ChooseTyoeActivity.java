1e7532c9c2b23a1b2d9a3ddcd139d3697e4e8645
vuducminh <vuducminhkyle@gmail.com>
2021-03-24 21:52:24 +0700

    [NEW] Thêm màn hình lựa chọn loại người dùng, màn hình map

---
+++ android/app/src/main/java/com/vuducminh/carpooling/ChooseTyoeActivity.java
@@ -0,0 +1,14 @@
+package com.vuducminh.carpooling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseTyoeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_tyoe);
+    }
+}
