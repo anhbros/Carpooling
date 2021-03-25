d05e4dcbeac3e1e2a3a3ea1f95c2765882935f42
Anh nguyen <harrykai99@gmail.com>
2021-03-26 00:03:53 +0700

    modify router

--- backend/routes/boocking.js
+++ backend/routes/boocking.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 var mongojs = require("mongojs");
 
-var db = mongojs("mongodb://eman:eman@ds163181.mlab.com:63181/taxiapp", ["bookings"]);
+//var db = mongojs("mongodb://eman:eman@ds163181.mlab.com:63181/taxiapp", ["bookings"]);
 
 router.get("/bookings", function(req, res, next){
 	db.bookings.find(function(err, bookings){
